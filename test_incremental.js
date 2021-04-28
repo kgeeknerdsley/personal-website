9bdea7ab4e9090174798b5bee9c0354c0d36b52d
Kevin Worsley <kgeekmail@gmail.com>
2021-04-28 13:46:21 -0700

    Auto increment money

--- test_incremental.js
+++ test_incremental.js
@@ -1,6 +1,13 @@
 var money = 0;
 
+
+//button click to make money
 function incrementMoney(incrementVal) {
     money += incrementVal;
     document.getElementById("moneyCount").innerHTML = money;
 }
+
+//game loop
+window.setInterval(function() {
+    incrementMoney(1);
+},1000);
